24cc241e43e79df6950149e3a8be5619c6a9478c
DianaDob <dobsinszki.diana@gmail.com>
2022-07-02 14:31:30 +0200

    feat: add students and override compare to

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -10,14 +10,33 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @Builder
-public class Room {
+public class Room implements Comparable<Room>{
     private int roomId;
-    //private Set<Student> students;
+    private Set<Student> students;
     private HouseType houseType;
 
-    public Room(int roomId, HouseType houseType) {
+    public Room(int roomId, Set<Student> students, HouseType houseType) {
         this.roomId = roomId;
-        //this.students = students;
+        this.students = students;
         this.houseType = houseType;
+    }
+
+    @Override
+    public int compareTo(Room o) {
+        if (this.roomId > o.roomId) {
+
+            // if current object is greater,then return 1
+            return 1;
+        }
+        else if (this.roomId < o.roomId) {
+
+            // if current object is greater,then return -1
+            return -1;
+        }
+        else {
+
+            // if current object is equal to o,then return 0
+            return 0;
+        }
     }
 }
