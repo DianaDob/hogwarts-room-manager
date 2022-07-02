da2c1cc28451fc7fbf4effdbf4d6d13c525b53c3
DianaDob <dobsinszki.diana@gmail.com>
2022-07-02 14:33:46 +0200

    feat: add students to rooms at initialization

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -1,9 +1,8 @@
 package com.codecool.hogwartshouses.data_sample;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
 import com.codecool.hogwartshouses.model.types.HouseType;
-import com.codecool.hogwartshouses.service.DAO.RoomMemory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -15,29 +14,43 @@ public class RoomCreator {
 
 
 
-    /*@Bean
-    public Set<Room> getRooms(){
-        Set<Room> rooms = new HashSet<>();
-        rooms.add(Room.builder().roomId(2).houseType(HouseType.HUFFLEPUFF).build());
-        rooms.add(new Room(1, HouseType.GRYFFINDOR));
-        return rooms;
-    }*/
+
+    private final Set<Student> allStudents;
+    private static final int ROOM_CAPACITY = 4;
+
+
 
     @Bean
     public Set<Room> initializeRooms() {
         Set<Room> rooms = new HashSet<>();
-        rooms.add(Room.builder().roomId(1).houseType(HouseType.GRYFFINDOR).build());
-        rooms.add(Room.builder().roomId(2).houseType(HouseType.HUFFLEPUFF).build());
-        rooms.add(Room.builder().roomId(3).houseType(HouseType.RAVENCLAW).build());
-        rooms.add(Room.builder().roomId(4).houseType(HouseType.SLYTHERIN).build());
+        Set<Student> gryffindorStudents = createStudentSetForRoom(HouseType.GRYFFINDOR);
+        Set<Student> hufflepuffStudents = createStudentSetForRoom(HouseType.HUFFLEPUFF);
+        Set<Student> ravenclawStudents = createStudentSetForRoom(HouseType.RAVENCLAW);
+        Set<Student> slytherinStudents = createStudentSetForRoom(HouseType.HUFFLEPUFF);
+
+        rooms.add(Room.builder().roomId(1).students(gryffindorStudents).houseType(HouseType.GRYFFINDOR).build());
+        rooms.add(Room.builder().roomId(2).students(hufflepuffStudents).houseType(HouseType.HUFFLEPUFF).build());
+        rooms.add(Room.builder().roomId(3).students(ravenclawStudents).houseType(HouseType.RAVENCLAW).build());
+        rooms.add(Room.builder().roomId(4).students(slytherinStudents).houseType(HouseType.SLYTHERIN).build());
         return rooms;
     //TODO
     }
 
 
-    /*public RoomCreator() {
-        initializeRooms();
-    }*/
+
+    public RoomCreator(Set<Student> allStudents) {
+        this.allStudents = allStudents;
+    }
+
+    private Set<Student> createStudentSetForRoom(HouseType houseType){
+        Set<Student> students = new HashSet<>(ROOM_CAPACITY);
+        for(Student student : allStudents){
+            if(student.getHouseType().equals(houseType)){
+               students.add(student);
+            }
+        }
+        return students;
+    }
 
 
 }
